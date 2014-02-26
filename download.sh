e67203e398b97570d1b7a0913d9de4ef95d6ffbb
Jinze Gu <guxxx220@berkeley.edu>
2014-02-26 10:32:40 -0800

    Only use for download

--- download.sh
+++ download.sh
@@ -1,13 +1,12 @@
+#! /bin/sh
+
 function dl(){
     grep href\= ~/Capstone/data|cut -d'"' -f4|grep \.csv|cut -d'/' -f7 > filename
     name=$(grep href\= ~/Capstone/data|cut -d'"' -f4|grep \.csv|cut -d'/' -f7)
-    for i in $name 
-    do 
+   for i in $name 
+     do 
        curl -O -k https://raw2.github.com/umbrae/reddit-top-2.5-million/master/data/$i
        echo "You Have downloaded "$i
-       newname=$i-"new"
-       cut -d',' -f1,4,5,6-9,11,15,19 $i>$newname|mv $newname ~/Capstone/MetaData/$i
-       rm ~/Capstone/$i-"new"
-       rm ~/Capstone/$i
-    done
+       mv $i ~/Capstone/MetaData/$i
+     done
 }
